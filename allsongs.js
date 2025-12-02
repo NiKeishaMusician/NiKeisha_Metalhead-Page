ac6294c96610b284e37b8626e1abcff413cc3e57
NiKeisha <keisanikola8@gmail.com>
2025-12-02 10:38:19 +0200

    aaa

--- allsongs.js
+++ allsongs.js
@@ -1,21 +1,94 @@
-// index.js
+// allsongs.js
 let lyrics = [
+  //ac/dc
   {
-    text: "You've been ____",
+    text: "(AC/DC) You've been ____",
     correct: "thunderstruck"
   },
   {
-    text: "Nothing else ____ ",
-    correct: "matters"
+    text: "Back in black, I hit the ____ ",
+    correct: "sack"
   },
   {
-    text: "We're not gonna ____ it (Twisted Sister - We're Not Gonna Take It)",
-    correct: "take"
+    text: "I'm on a highway ____",
+    correct: "to hell"
   },
   {
-    text: "You give love a bad ____ (Bon Jovi - You Give Love a Bad Name)",
-    correct: "name"
-  }
+    text: "Cause I'm T.N.T., I'm ____",
+    correct: "dynamite"
+  },
+  { text: "You shook me _____",
+    correct: "all night long"
+  },
+  {
+    text: "____ train, Runnin' right off the track",
+    correct: "Runaway"
+  },
+  {
+    text: "You got me ringin', ____",
+    correct: "Hells Bells"
+  },
+  {
+    text: "Dirty deeds done ____",
+    correct: "dirt cheap"
+  },
+  {
+    text: "Who made who? Who turned the ____?",
+    correct: "screw"
+  },
+  {
+    text: "It's a ____ to the top",
+    correct: "long way"
+  },
+
+  //blacksabbath
+  {
+    text: "(Band - Black Sabbath) I am ____",
+    correct: "Iron Man"
+  },
+{
+  text: '(Band - Black Sabbath) "Generals gathered in their masses"',
+  correct: "War Pigs"
+},
+{
+  text: "(Band - Black Sabbath) Can you help me, ____?",
+  correct: "occupy my brain"
+},
+{
+  text: '(Band - Black Sabbath) "My name is Lucifer, ____"',
+  correct: "please take my hand"
+},
+{
+  text: '(Band - Black Sabbath) "The lover of life ____"',
+  correct: "is not a sinner"
+},
+{
+  text: '(Band - Black Sabbath) "Revolution in their ____"',
+  correct: "minds"
+},
+{
+  text: '(Band - Black Sabbath) "Reflex in the sky warns you, ____"',
+  correct: "you are gonna die"
+},
+{
+  text: '(Band - Black Sabbath) "What is this that stands ____?"',
+  correct: "before me"
+},
+{
+  text: '(Band - Black Sabbath) "____ and you gotta believe me"',
+  correct: "Fairies Wear Boots"
+},
+{
+  text: '(Band - Black Sabbath) "Rocket engines ____ so fast"',
+  correct: "burning fuel"
+}
+
+  //cannibalcorpse
+
+  
+
+
+
 ];
 
 
@@ -43,7 +116,7 @@ function checkAnswer() {
 
 
   if (userAnswer === currentLyric.correct.toLowerCase()) {
-    message.textContent = "🔥 Correct! You rock! 🔥";
+    message.textContent = "🔥 Correct! You rock!🔥";
     message.style.color = "limegreen";
   } else {
     message.textContent = `Uh oh! Try again with this"${currentLyric.correct}".`;
